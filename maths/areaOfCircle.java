b45bee86d9032cf59924ed581a0ac371325856c2
Asmaul Mallick <asmaulmallick360@gmail.com>
2026-01-23 23:34:02 +0530

    23/1/26

--- maths/areaOfCircle.java
+++ maths/areaOfCircle.java
@@ -1,5 +1,35 @@
+/*
+Question:
+Write a Java program to calculate the area of a circle.
+The program should take the radius as input from the user and compute the area.
+
+Formula:
+Area = π × r × r
+
+Example:
+Input:
+Radius = 5
+Calculation:
+Area = 3.17 × 5 × 5 = 79.25
+Output:
+The area of circle is: 79.25
+*/
+
 package maths;
+import java.util.Scanner;
 
 public class areaOfCircle {
-  
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter the value of radius : ");
+    int r = in.nextInt();
+
+    double pie = 3.17;
+    double circle = pie * (r * r); 
+
+    System.out.println("The area of circle is : " + circle);
+
+    in.close();
+  }
 }
