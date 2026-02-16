7c269992088e304075face7c1c54f5edd81d1bdc
Asmaul Mallick <asmaulmallick360@gmail.com>
2026-02-16 20:24:13 +0530

    arithmetic progression

---
+++ maths/arithmeticProgression.java
@@ -0,0 +1,38 @@
+/*
+ Question:
+ Write a Java program to find the sum of an Arithmetic Progression (AP).
+
+ The user will enter:
+ - First term (a)
+ - Common difference (d)
+ - Number of terms (n)
+
+ Find the sum using the formula:
+ S = n/2 × [2a + (n-1)d]
+*/
+
+package maths;
+
+import java.util.Scanner;
+
+public class ArithmeticProgression {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter first term (a): ");
+    int a = sc.nextInt();
+
+    System.out.print("Enter common difference (d): ");
+    int d = sc.nextInt();
+
+    System.out.print("Enter number of terms (n): ");
+    int n = sc.nextInt();
+
+    int sum = n * (2 * a + (n - 1) * d) / 2;
+
+    System.out.println("Sum of AP = " + sum);
+
+    sc.close();
+  }
+}
